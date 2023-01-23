a6ace9beaf89035e89c6509b253c9e6c856e2c31
Andres Chavez <andres.chaves@workrise.com>
2023-01-23 15:51:01 -0500

    Simple particles multicolor mouse trail

--- script.js
+++ script.js
@@ -71,9 +71,9 @@ const handleParticles = () => {
 };
 
 const animate = () => {
-  // ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "rgba(0,0,0,0.1)";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // ctx.fillStyle = "rgba(0,0,0,0.1)";
+  // ctx.fillRect(0, 0, canvas.width, canvas.height);
   handleParticles();
   hue += 5;
 
